207a46c4827ca25333b435f15a5c9515f113ba58
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-03-14 13:15:13 -0400

    Create DELTE route and exception

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :create]
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    end
+  end
 end
