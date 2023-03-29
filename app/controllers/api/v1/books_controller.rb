13afe6afcceadeb8ca1616e966ca032cad1563d2
Selam Chaka <selblessed@gmail.com>
2023-03-29 11:58:16 -0400

    adding modified specs

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,14 +2,22 @@ module Api
   module V1
     class BooksController < ApplicationController
       
-      def index
-        render json: Book.all
-      end
+      # def index
+      #   render json: Book.all
+      # end
 
-      def index_titleid
-        render json: Book.find(params[:title][:id])
+      def index
+        if params
+          render json: Book.find(params[:title][:id])
+        else
+          render json: Book.all
+        end
       end
       
+      def allowed_params
+        params.permit(:title, :id)
+      end
+
       def create
         # book= Book.new(title:params[:title],author:params[:author])
         book= Book.new(book_params)
@@ -31,7 +39,7 @@ module Api
       def book_params
         params.require(:book).permit(:title, :author)
       end
-
+    
     end
   end
 end
