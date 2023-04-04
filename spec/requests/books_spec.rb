18204d6d1c20a2ba2a7adce796e86e354911a9bd
Selam Chaka <selblessed@gmail.com>
2023-04-04 10:24:29 -0400

    adding all tests passed and adding one query param rspec

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -7,16 +7,14 @@ describe 'Books API', type: :request do
         let!(:book1) { FactoryBot.create(:book, title:'The Martian', author: 'Andy Weir') }
         let!(:book2) { FactoryBot.create(:book, title:'Night', author: 'Elie Wiesel') }
         it "return all books" do 
-            # binding.pry
+            # binding.pry 
             get '/api/v1/books'
             res = JSON.parse(response.body)
-            # puts res
             expect(response).to have_http_status(:ok)
             expect(res.length).to eq(Book.count) 
-            
-            # expect(res[0]).to eq(book.as_json)
-            # expect(res[1]).to eq(book1.as_json)
-            # expect(res[2]).to eq(book2.as_json)
+            expect(res[0]).to eq(book.as_json)
+            expect(res[1]).to eq(book1.as_json)
+            expect(res[2]).to eq(book2.as_json)
         end
     end
 
@@ -32,17 +30,28 @@ describe 'Books API', type: :request do
                 get "/api/v1/books?title=#{book.title}&id=#{book.id}"
                 res = JSON.parse(response.body)
                 expect(response).to have_http_status(:ok)
-                # expect(res).to eq(book.as_json)
-                expect(res["title"]).to eq(book.title)
-                expect(res["id"]).to eq(book.id)
-                expect(res["author"]).to eq(book.author)
-                expect(res).to have_key("created_at")
-                expect(res).to have_key("updated_at")
-
-
+                expect(res).to eq(book.as_json)
             end
         end
     end
+
+    describe 'GET /books?title', appmap: true do
+        let!(:book) { FactoryBot.create(:book, title:'The Martian', author: 'Andy Weir') }
+        let!(:book1) { FactoryBot.create(:book, title:'Night', author: 'Elie Wiesel') }
+        context 'when we get a book by title' do
+            it "returns not found with a missing title" do
+                get "/api/v1/books?title=Hello"
+                expect(response).to have_http_status(:not_found)
+            end
+            it "returns a book by title" do
+                get "/api/v1/books?title=#{book.title}"
+                res = JSON.parse(response.body)
+                expect(response).to have_http_status(:ok)
+                expect(res).to eq(book.as_json)
+            end
+        end
+    end
+
 
     describe 'POST /books', appmap: true do
         context 'when create a book' do
@@ -62,7 +71,6 @@ describe 'Books API', type: :request do
                 delete "/api/v1/books/#{book.id}", params: {}
                 expect(response).to have_http_status(:no_content)
                 expect(response.body).to be_empty
-                # expect(response).to have_http_status(:ok)
                 expect(Book.exists?(book.id)).to be false
                 expect(Book.count).to eq 0
             end
